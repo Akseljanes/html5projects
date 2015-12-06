b82fcc6e7dcfb2cc7aacdb077d8ad0bbfa1b4b79
Aksel Jänes <aksel.janes@khk.ee>
2015-12-06 23:38:50 +0200

    Lecture 41: PHP upload file Part - A [Delivers #109454826]

--- upload.php
+++ upload.php
@@ -1,7 +1,17 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Aksel
- * Date: 06/12/15
- * Time: 23:24
- */
+$upload_dir = 'uploads/';
+$allowed_ext = array('jpg', 'jpeg', 'png', 'gif');
+if (array_key_exists('pic', $_FILES && $_FILES['pic']['error'] == 0)) {
+    $pic = $_FILES['pic'];
+    if (!in_array(get_extension($pic['name']), $allowed_ext)) {
+        exit_status('Only ' . implode(',', $allowed_ext) . ' files are allowed');
+    }
+    if (move_uploaded_files($pic['tmp_name'], $upload_dir . $pic['name'])) {
+        exit_status('File uploaded');
+    }
+}
+//Helpers
+function exit_status($str) {
+    echo json_encode(array('status'=>$str));
+}
+?>
