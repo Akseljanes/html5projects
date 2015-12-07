14bffc119070053b26efc8702c4b7caba391949e
Aksel Jänes <aksel.janes@khk.ee>
2015-12-07 03:16:09 +0200

    Lecture 47: Process Questions [Delivers #109454946]

--- js/quiz.js
+++ js/quiz.js
@@ -14,38 +14,77 @@ function init() {
 
 $(document).ready(function(){
     $('.questionForm').hide();
-
     $('#q1').show();
 
     $('#q1 #submit').click(function(){
         $('.questionForm').hide();
+        process('q1')
         $('#q2').fadeIn(300);
         return false
     });
 
     $('#q2 #submit').click(function(){
         $('.questionForm').hide();
+        process('q2')
         $('#q3').fadeIn(300);
         return false
     });
 
     $('#q3 #submit').click(function(){
         $('.questionForm').hide();
+        process('q3')
         $('#q4').fadeIn(300);
         return false
     });
 
     $('#q4 #submit').click(function(){
         $('.questionForm').hide();
+        process('q4')
         $('#q5').fadeIn(300);
         return false
     });
 
     $('#q5 #submit').click(function(){
         $('.questionForm').hide();
+        process('q5')
         $('#results').fadeIn(300);
         return false
     });
 });
+
+function process(q) {
+    if(q == "q1") {
+        var submitted = $('input[name=q1]:checked').val();
+        if(submitted == sessionStorage.a1){
+            score++;
+        }
+    }
+    if(q == "q2") {
+        var submitted = $('input[name=q2]:checked').val();
+        if(submitted == sessionStorage.a2){
+            score++;
+        }
+    }
+    if(q == "q3") {
+        var submitted = $('input[name=q3]:checked').val();
+        if(submitted == sessionStorage.a3){
+            score++;
+        }
+    }
+    if(q == "q4") {
+        var submitted = $('input[name=q4]:checked').val();
+        if(submitted == sessionStorage.a4){
+            score++;
+        }
+    }
+    if(q == "q5") {
+        var submitted = $('input[name=q5]:checked').val();
+        if(submitted == sessionStorage.a5){
+            score++;
+        }
+        $('#results').html('<h3>Youre final score: ' + score +' out of ' + highest + '</h3>');
+    }
+    return false;
+}
 
 window.addEventListener('load',init,false);
