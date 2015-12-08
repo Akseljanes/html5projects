fd38de73bb5e89ce4605e50dec2640597bdee03d
Aksel Jänes <aksel.janes@khk.ee>
2015-12-08 20:14:40 +0200

    Lecture 68: Database [Delivers #109462442]

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
-/**
- * Created by Aksel on 08/12/15.
- */
+$(document).ready(function(){
+    //Open Database
+    var request = indexedDB.open('customermanager',1);
+
+    request.onupgradeneeded = function(e){
+        var db = e.target.result;
+
+        if(!db.objectStoreNames.contains('customers')){
+            var os = db.createObjectStore('customers',{keyPath: "id", autoIncrement:true});
+
+        }
+    }
+
+    //Success
+    request.onsuccess = function(e){
+        console.log('Success: Opened Database...');
+        db = e.target.result;
+        //Show Customers
+        //showCustomers();
+    }
+
+    //Error
+    request.onerror = function(e){
+        console.log('Error: Could Not Open Database...');
+    }
+});
