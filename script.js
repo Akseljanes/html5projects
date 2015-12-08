13cf7910cb6a58ca0969b99f8088f9ab6ee0d80e
Aksel Jänes <aksel.janes@khk.ee>
2015-12-08 20:38:20 +0200

    Lecture 73: Get User Location [Delivers #109462536]

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
-/**
- * Created by Aksel on 08/12/15.
- */
+var startPos;
+var watchId;
+
+function startTracking(){
+    if(navigator.geolocation){
+        document.getElementById('startBtn').style.display = 'none';
+        document.getElementById('stopBtn').style.display = 'inline';
+        //Get Position
+        navigator.geolocation.getCurrentPosition(showPosition, showError);
+        //Watch Position
+        //watchId = navigator.geolocation.watchPosition(showPositionUpdate,showError);
+    } else {
+        alert('Geolocation is not supported by your browser');
+    }
+}
+
+function showPosition(position){
+    startPos = position;
+    document.getElementById('startLat').innerHTML = startPos.coords.latitude;
+    document.getElementById('startLon').innerHTML = startPos.coords.longitude;
+}
+
+function showError(error){
+
+}
